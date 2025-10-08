d271e8a970e38a37915b1187fff187c342e45cf3
Mattha212 <wyzmatt@gmail.com>
2025-10-08 14:39:04 +0200

    add html and pj-sheet

--- mysystem.mjs
+++ mysystem.mjs
@@ -1,1 +1,24 @@
 console.log("Mon système est chargé !");
+
+class PJSheet extends ActorSheet {
+    static get defaultOptions() {
+        return mergeObject( super.defaultOptions, {
+            classes: ["testsystem","sheet","actor"],
+            template: "systems/testsystem/templates/pj-sheet.html",
+            width: 500,
+            height: 300
+        });
+    }
+    getData(options){
+        const context = super.getData(options);
+        return context;
+    }
+}
+Hooks.once("init", ()=>{
+    console.log("Test System | Initialization...");
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("testsystem", PJSheet, {
+    types: ["PJ"],
+    makeDefault: true
+    });
+});
