7c2a4256da8147c4c22d7a4c6f9cb1953d07c70f
Mattha212 <wyzmatt@gmail.com>
2025-10-09 15:04:37 +0200

    add object sheet template

--- mysystem.mjs
+++ mysystem.mjs
@@ -22,13 +22,41 @@ class PJSheet extends ActorSheet {
             initial: "stats"
         });
         this._tabs["primary"].bind(html[0]);
+
+        html.find(".item-name").click(ev => {
+            const li = $(ev.currentTarget).closest(".item");
+            const item = this.actor.items.get(li.data("itemId"));
+            item.sheet.render(true);
+        });
     }
 }
+
+class ObjectSheet extends ItemSheet{
+    static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["testsystem", "sheet", "item"],
+      template: "systems/testsystem/templates/object-sheet.html",
+      width: 400,
+      height: 300
+    });
+  }
+
+  getData(options) {
+    const context = super.getData(options);
+    return context;
+  }
+}
+
 Hooks.once("init", ()=>{
     console.log("Test System | Initialization...");
     Actors.unregisterSheet("core", ActorSheet);
     Actors.registerSheet("testsystem", PJSheet, {
     types: ["PJ"],
     makeDefault: true
+    });
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("testsystem", ObjectSheet, {
+        types:["Object"],
+        makeDefault:true
     });
 });
