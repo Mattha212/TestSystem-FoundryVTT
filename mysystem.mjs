fc7212dddeeb221d26358bf3d877a91b5d6bdaf9
Mattha212 <wyzmatt@gmail.com>
2025-10-28 15:21:52 +0100

    add link between character stats

--- mysystem.mjs
+++ mysystem.mjs
@@ -42,7 +42,6 @@ class PJSheet extends ActorSheet {
     }
 
     async _onChangeInput(event){
-        await super._onChangeInput(event);
         const input = event.target;
 
         if(input.name?.endsWith(".MaxValue")){
@@ -53,8 +52,20 @@ class PJSheet extends ActorSheet {
             const maxValue = this.actor.system.stats[statKey].MaxValue;
             const label = this.actor.system.stats[statKey].Label;
             const update={};
-            if(label === "Constitution"){
 
+            if(label === "Constitution"){
+                const currentValueStrength = this.actor.system.stats["Strength"].CurrentValue;
+                const currentValueAgility = this.actor.system.stats["Agility"].CurrentValue;
+                if(newValue<=maxValue*0.75 && newValue>maxValue*0.5){
+                    update[`system.stats.${Agility}.CurrentValue`] = currentValueAgility - 10;
+                }
+                else if(newValue<=maxValue*0.5 && newValue>maxValue*0.25){
+                    update[`system.stats.${Strength}.CurrentValue`] = currentValueStrength -10;
+                }
+                else if(newValue<=maxValue*0.25 && newValue>0){
+                    update[`system.stats.${Agility}.CurrentValue`] = currentValueAgility - 10;
+                    update[`system.stats.${Strength}.CurrentValue`] = currentValueStrength -10;
+                }
             }
             else{
             update[`system.stats.${statKey}.MaxValue`] = newValue;
@@ -62,6 +73,9 @@ class PJSheet extends ActorSheet {
             }
             await this.actor.update(update);
         }
+        
+        await super._onChangeInput(event);
+
     }
 }
 
