e2125fefc6fa95bd786aa19a5527ebd037b84f63
Mattha212 <wyzmatt@gmail.com>
2025-11-23 18:27:54 +0100

    update PJsheet for v13

--- mysystem.mjs
+++ mysystem.mjs
@@ -70,28 +70,29 @@ class PJSheet extends foundry.applications.api.HandlebarsApplicationMixin(foundr
 	}
 
     static _onClickTab(event) {
-           event.preventDefault();
+        console.log(this);
+    // event.preventDefault();
 
-    const target = event.target;
-    const tab = target.dataset.tab;
-    const group = target.closest(".tabs").dataset.group;
+    // const target = event.target;
+    // const tab = target.dataset.tab;
+    // const group = target.closest(".tabs").dataset.group;
 
-    // 1. Trouver l'élément porteur de data-appid
-    const appRoot = target.closest("[data-appid]");
-    if (!appRoot) return;
+    // // 1. Trouver l'élément porteur de data-appid
+    // const appRoot = target.closest("[data-appid]");
+    // if (!appRoot) return;
 
-    // 2. ID interne de l’app
-    const appId = appRoot.dataset.appid;
+    // // 2. ID interne de l’app
+    // const appId = appRoot.dataset.appid;
 
-    // 3. Récupérer l’instance PJSheet
-    const sheet = foundry.applications.instances[appId];
-    if (!sheet) return;
+    // // 3. Récupérer l’instance PJSheet
+    // const sheet = foundry.applications.instances[appId];
+    // if (!sheet) return;
 
-    // 4. Mettre à jour tabGroups
-    sheet.tabGroups[group] = tab;
+    // // 4. Mettre à jour tabGroups
+    // sheet.tabGroups[group] = tab;
 
-    // 5. Re-render
-    sheet.render();
+    // // 5. Re-render
+    // sheet.render();
     }
 
     async _prepareContext(options){
