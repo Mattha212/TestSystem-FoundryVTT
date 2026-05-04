05795552c28c3e054e62f91145d10fb33d2749d2
Mattha212 <wyzmatt@gmail.com>
2026-05-04 14:34:11 +0200

    add ingredients and recipe

--- mysystem.mjs
+++ mysystem.mjs
@@ -2863,6 +2863,8 @@ class RecipeSheet extends ObjectsItemsSheet {
         const parsed = JSON.parse(dataString);
         const item = await fromUuid(parsed.uuid);
         if (!item) return;
+        if (item.type != "Crafting Component") return;
+
         itemData = {
             name: item.name || "Unnamed Item",
             type: item.system.type || "no type",
